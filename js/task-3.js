366046dfbfb6b44291566ffe2906a95572819856
Max M <maxminullin@gmail.com>
2021-02-09 09:17:55 +0200

    task3

---
+++ js/task-3.js
@@ -0,0 +1,34 @@
+const Storage = function (items) {
+  this.items = items;
+}
+
+Storage.prototype.getItems = function (items) {
+  return this.items;
+};
+
+Storage.prototype.addItem = function (item) {
+  this.items.push(item)
+  return this.items;
+}
+
+Storage.prototype.removeItem = function (item) {
+  this.items.
+}
+
+
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
